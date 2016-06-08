af1a9a9911bade4569b0c21c4cc913785f435088
John <CodeMorrow@yahoo.com>
2016-06-08 21:31:31 +0000

    Unique Element Complete

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,15 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    unique=[]       
+    counts = Hash.new(0)
+       arr.each do |i|
+           counts[i] += 1
+       end
+       counts.each do |k, v|
+           if v == 1
+              unique.push(k)
+           end
+       end
+    return unique
 end
